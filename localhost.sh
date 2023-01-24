c42d0f87c5be070da5fda562b64a2cc10356c031
Hydrogen-27 <118648351+Hydrogen-27@users.noreply.github.com>
2023-01-24 21:28:36 +0800

    Update localhost.sh

--- localhost.sh
+++ localhost.sh
@@ -17,45 +17,6 @@ echo ${HEX_00cc00}
 ## Clean
 clear
 
-## PHP
-cat <<- EOF
-		${HEX_0000cc}░█▀█░█░█░█▀█░
-${HEX_0000cc}░█▀▀░█▀█░█▀▀░
-${HEX_0000cc}░▀░░░▀░▀░▀░░░ ${HEX_ffffff}Version: ${HEX_ff0000}$PHP_v${HEX_ffffff}
-${HEX_ffffff}
-EOF
-echo ${HEX_ffffff}"Installing PHP..."
-sleep 5
-echo 
-pkg install php
-sleep 5
-
-## Clean
-clear
-
-## Main Banner
-cat <<- EOF
-		${HEX_00cc00}░█░░░█▀█░█▀▀░█▀█░█░░░█░█░█▀█░█▀▀░▀█▀░
-${HEX_00cc00}░█░░░█░█░█░░░█▀█░█░░░█▀█░█░█░▀▀█░░█░░
-${HEX_00cc00}░▀▀▀░▀▀▀░▀▀▀░▀░▀░▀▀▀░▀░▀░▀▀▀░▀▀▀░░▀░░
-${HEX_ffffff}Tool Created by: ${HEX_5dd5fc}Hydrogen-27
-${HEX_ffffff}
-EOF
-
-## Usage
-echo "${HEX_0000cc}Usage${HEX_ffffff}"
-A="${HEX_ff0000}• ${HEX_ffffff}cd storage/shared/localhost"
-B="${HEX_ff0000}• ${HEX_ffffff}bash localhost.sh"
-C="${HEX_ff0000}• ${HEX_ffffff}Enter Host and Enter Port"
-echo $A
-echo $B
-echo $C
-echo
-
-## SG
-## Continue
-read -p "Enter to Continue: " && sleep 1 && clear
-
 ## Main Banner
 cat <<- EOF
 		${HEX_00cc00}░█░░░█▀█░█▀▀░█▀█░█░░░█░█░█▀█░█▀▀░▀█▀░
